2fc7fab2685371304bb042b32468eda5da09c707
Rui Santos <ee11077@fe.up.pt>
2024-03-21 11:00:18 +0000

    Update ESP32_TFT_Touchscreen_Basic.ino

--- examples/ESP32_TFT_Touchscreen_Basic/ESP32_TFT_Touchscreen_Basic.ino
+++ examples/ESP32_TFT_Touchscreen_Basic/ESP32_TFT_Touchscreen_Basic.ino
@@ -1,30 +1,19 @@
-/* 
-  Rui Santos & Sara Santos - Random Nerd Tutorials
-
-  THIS EXAMPLE WAS TESTED WITH THE FOLLOWING HARDWARE:
-  1) ESP32-2432S028R 2.8 inch 240×320 also known as the Cheap Yellow Display (CYD):
-    https://makeradvisor.com/tools/cyd-cheap-yellow-display-esp32-2432s028r/
-    SET UP INSTRUCTIONS: https://RandomNerdTutorials.com/cyd/
-
-  2) REGULAR ESP32 Dev Board + 2.8 inch 240x320 TFT Display:
-    https://makeradvisor.com/tools/2-8-inch-ili9341-tft-240x320/
-    https://makeradvisor.com/tools/esp32-dev-board-wi-fi-bluetooth/
-    SET UP INSTRUCTIONS: https://RandomNerdTutorials.com/esp32-tft/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files. 
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+/*  Rui Santos & Sara Santos - Random Nerd Tutorials
+    THIS EXAMPLE WAS TESTED WITH THE FOLLOWING HARDWARE:
+    1) ESP32-2432S028R 2.8 inch 240×320 also known as the Cheap Yellow Display (CYD): https://makeradvisor.com/tools/cyd-cheap-yellow-display-esp32-2432s028r/
+      SET UP INSTRUCTIONS: https://RandomNerdTutorials.com/cyd/
+    2) REGULAR ESP32 Dev Board + 2.8 inch 240x320 TFT Display: https://makeradvisor.com/tools/2-8-inch-ili9341-tft-240x320/ and https://makeradvisor.com/tools/esp32-dev-board-wi-fi-bluetooth/
+      SET UP INSTRUCTIONS: https://RandomNerdTutorials.com/esp32-tft/
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
 #include <SPI.h>
 
-/*
-  Install the "TFT_eSPI" lbirary by Bodmer to interface with the TFT Display - https://github.com/Bodmer/TFT_eSPI
-  *** IMPORTANT: User_Config.h available on the internet will probably NOT work with the examples available at Random Nerd Tutorials ***
-  *** YOU MUST USE THE User_Config.h FILE PROVIDED IN THE LINK BELOW IN ORDER TO USE THE EXAMPLES FROM RANDOM NERD TUTORIALS ***
-  https://RandomNerdTutorials.com/cyd/
-  https://RandomNerdTutorials.com/esp32-tft/
-  FULL INSTRUCTIONS AVAILABLE ON HOW CONFIGURE THE LIBRARY: https://RandomNerdTutorials.com/cyd/ or https://RandomNerdTutorials.com/esp32-tft/
-*/
+/*  Install the "TFT_eSPI" lbirary by Bodmer to interface with the TFT Display - https://github.com/Bodmer/TFT_eSPI
+    *** IMPORTANT: User_Config.h available on the internet will probably NOT work with the examples available at Random Nerd Tutorials ***
+    *** YOU MUST USE THE User_Config.h FILE PROVIDED IN THE LINK BELOW IN ORDER TO USE THE EXAMPLES FROM RANDOM NERD TUTORIALS ***
+    FULL INSTRUCTIONS AVAILABLE ON HOW CONFIGURE THE LIBRARY: https://RandomNerdTutorials.com/cyd/ or https://RandomNerdTutorials.com/esp32-tft/   */
 #include <TFT_eSPI.h>
 
 // Install the "XPT2046_Touchscreen" library by Paul Stoffregen to use the Touchscreen - https://github.com/PaulStoffregen/XPT2046_Touchscreen
