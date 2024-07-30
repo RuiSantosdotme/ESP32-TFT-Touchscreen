9f207afbfc149ca26513ccd9fa9cb1ff187da10e
Rui Santos <randomnerdtutorials@gmail.com>
2024-07-30 17:28:00 +0100

    Update User_Setup.h

--- configs/User_Setup.h
+++ configs/User_Setup.h
@@ -214,13 +214,13 @@
 // For ESP32 Dev board (only tested with ILI9341 display)
 // The hardware SPI can be mapped to any pins
 
-#define TFT_MISO 16
+#define TFT_MISO 12
 #define TFT_MOSI 13
 #define TFT_SCLK 14
 #define TFT_CS   15  // Chip select control pin
 #define TFT_DC    2  // Data Command control pin
 //#define TFT_RST   4  // Reset pin (could connect to RST pin)
-#define TFT_RST  12  // Set TFT_RST to -1 if display RESET is connected to ESP32 board RST
+#define TFT_RST  -1  // Set TFT_RST to -1 if display RESET is connected to ESP32 board RST
 
 #define TOUCH_CS 33     // Chip select pin (T_CS) of touch screen
 
