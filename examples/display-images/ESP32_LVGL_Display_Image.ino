ebf7623352f583d429007228e403ad69d53eb509
Rui Santos <randomnerdtutorials@gmail.com>
2024-09-02 15:34:31 +0100

    Update ESP32_LVGL_Display_Image.ino

--- examples/display-images/ESP32_LVGL_Display_Image.ino
+++ examples/display-images/ESP32_LVGL_Display_Image.ino
@@ -22,8 +22,8 @@
 
 #include <image.h>
 
-#define SCREEN_WIDTH 320
-#define SCREEN_HEIGHT 240
+#define SCREEN_WIDTH 240
+#define SCREEN_HEIGHT 320
 
 #define DRAW_BUF_SIZE (SCREEN_WIDTH * SCREEN_HEIGHT / 10 * (LV_COLOR_DEPTH / 8))
 uint32_t draw_buf[DRAW_BUF_SIZE / 4];
@@ -56,7 +56,8 @@ void setup() {
   lv_display_t * disp;
   // Initialize the TFT display using the TFT_eSPI library
   disp = lv_tft_espi_create(SCREEN_WIDTH, SCREEN_HEIGHT, draw_buf, sizeof(draw_buf));
-  
+  lv_display_set_rotation(disp, LV_DISPLAY_ROTATION_270);
+    
   // Function to draw the image
   draw_image();
 }
