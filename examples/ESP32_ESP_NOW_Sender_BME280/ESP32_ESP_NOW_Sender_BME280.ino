f48e254f1e743eb9cccaad90fe3c69fb9214a3a5
Rui Santos <randomnerdtutorials@gmail.com>
2025-07-10 14:18:09 +0100

    Create ESP32_ESP_NOW_Sender_BME280.ino

---
+++ examples/ESP32_ESP_NOW_Sender_BME280/ESP32_ESP_NOW_Sender_BME280.ino
@@ -0,0 +1,113 @@
+/*********
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/esp32-cyd-esp-now-receive-data/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*********/
+#include <esp_now.h>
+#include <WiFi.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_Sensor.h>
+
+// Set your Board ID (ESP32 Sender #1 = BOARD_ID 1, ESP32 Sender #2 = BOARD_ID 2, etc)
+#define BOARD_ID 1
+
+Adafruit_BME280 bme; 
+
+// REPLACE WITH YOUR ESP RECEIVER'S MAC ADDRESS
+uint8_t broadcastAddress[] = {0x24, 0xDC, 0xC3, 0x49, 0x6A, 0x14};
+
+// Structure to send data, must match the receiver structure
+typedef struct struct_message {
+    int id;
+    float temp;
+    float hum;
+    int readingId;
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+unsigned long previousMillis = 0;   // Stores last time temperature was published
+const long interval = 10000;        // Interval at which to publish sensor readings
+
+unsigned int readingId = 0;
+
+void initBME() {
+  if (!bme.begin(0x76)) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+}
+
+float readTemperature() {
+  float t = bme.readTemperature();
+  return t;
+}
+
+float readHumidity() {
+  float h = bme.readHumidity();
+  return h;
+}
+
+esp_now_peer_info_t peerInfo;
+
+// Callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  char macStr[18];
+  Serial.print("Packet to: ");
+  // Copies the sender mac address to a string
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print(macStr);
+  Serial.print(" send status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+ 
+void setup() {
+  Serial.begin(115200);
+  initBME(); 
+
+  WiFi.mode(WIFI_STA);
+ 
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  esp_now_register_send_cb(OnDataSent);
+   
+  // Register peer
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  // Copy receiver's MAC address to peerInfo
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
+ 
+void loop() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    // Save the last time a new reading was published
+    previousMillis = currentMillis;
+    // Set values to send
+    myData.id = BOARD_ID;
+    myData.temp = readTemperature();
+    myData.hum = readHumidity();
+    myData.readingId = readingId++;
+     
+    // Send data and check for errors
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+    Serial.print("Sending data (Reading ID: ");
+    Serial.print(myData.readingId);
+    Serial.print(", Temp: ");
+    Serial.print(myData.temp);
+    Serial.print("C, Hum: ");
+    Serial.print(myData.hum);
+    Serial.print("%): ");
+    Serial.println(result == ESP_OK ? "Sent" : "Failed to send");
+  }
+}
