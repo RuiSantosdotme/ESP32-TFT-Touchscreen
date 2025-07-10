c79ca91ea226c6b3b086c6988eea6512bcd4fa96
Rui Santos <randomnerdtutorials@gmail.com>
2025-07-10 14:42:31 +0100

    Update ESP32_ESP_NOW_Sender_BME280.ino

--- examples/ESP32_ESP_NOW_Sender_BME280/ESP32_ESP_NOW_Sender_BME280.ino
+++ examples/ESP32_ESP_NOW_Sender_BME280/ESP32_ESP_NOW_Sender_BME280.ino
@@ -56,7 +56,7 @@ esp_now_peer_info_t peerInfo;
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   char macStr[18];
   Serial.print("Packet to: ");
-  // Copies the sender mac address to a string
+  // Copies the receiver mac address to a string
   snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
            mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
   Serial.print(macStr);
